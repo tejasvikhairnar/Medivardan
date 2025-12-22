5d31ed683574d1da27321adcc3daa9941dd48562
Gagan06969 <gagandeepbhakuni@gmail.com>
2025-12-22 23:20:14 +0530

    to resolve 404 error corrected project directory

--- MEDI-VARDAAN-main/src/app/loading.js
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
-      <p>Loading the application...</p>
-    </div>
-  );
-}
